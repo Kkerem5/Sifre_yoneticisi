f8c19f45bde1e5144551bff84ca2a16a2c3e6273
kerem <144730694+Kkerem5@users.noreply.github.com>
2023-09-20 00:06:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 sifreler = {}
 karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 while True:
-    karar = input('Şifre oluşturmek için "s", kaydedilen şifrelere bakmak için "a" tuşuna basın, çıkmak için "cık" yazın: ')   
+    karar = input('Şifre oluşturmek için "s", kaydedilen şifrelere bakmak için "a",kaydedilen şifreleri ayarlamak için "f" tuşuna basın. çıkmak için "cık" yazın:')   
 
     if karar == 's': 
         while True:
@@ -25,6 +25,43 @@ while True:
             print('sifre')
     elif karar == 'a':
         print(sifreler)
+    elif karar == 'f':
+        while True:
+            if len(sifreler) == 0:
+                print('Kaydedilmiş şifre bulunamaktadır, çıkılıyor')
+                break
+            else:
+                while True:
+                    ayar_secimi = input('Bir "platform - şifreyi" silmek için "s", şifre değiştirmek için "d" tuşuna basınız, çıkmak için "cık"')
+                    if ayar_secimi == 's':
+                        print(sifreler)
+                        hangisi = input('Girmek istediğiniz platform ve şifrenin platformunu girin:')
+                        if hangisi in sifreler:
+                            del sifreler[hangisi]
+                            print('Güncel hali:', sifreler)
+                        elif hangisi == 'cık':
+                            break
+                        else:
+                            print('Girdiğiniz platform bulunamadı')
+                    elif ayar_secimi == 'd':
+                        while True:
+                            de_platform = input('Hangi platfromun şifresini değiştirmek istiyorsunuz?, çıkmak için "cık"')
+                            if de_platform in sifreler:
+                                yeni_sifre_karar = input('Şifresini kendiniz değiştirmek için "a", otomatik bir şifre için "d" ye basınız')
+                                if yeni_sifre_karar == 'a':
+                                    yeni_sifre = input("Yeni şifrenizi girin:")
+                                    sifreler[de_platform] = yeni_sifre
+                                elif yeni_sifre_karar == 'd':
+                                    yeni_uzunluk = int(input('Şifrenizin uzunluğu kaç olsun?'))
+                                    for i in range(yeni_uzunluk):
+                                        yeni_bir_sifre = sifre + random.choice(karakterler)
+                                    print(yeni_bir_sifre)
+                                    sifreler[de_platform] = yeni_bir_sifre
+                                    print(sifreler)
+                            elif de_platform == 'cık':
+                                break
+                    elif ayar_secimi == 'cık':
+                        break
     elif karar == 'cık':
         break
     else:
